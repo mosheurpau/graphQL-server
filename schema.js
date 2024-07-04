cb625e0ea98ce7a81cdbe4a8ea25318ec64b704e
mosheurpau <mosheur.pau@gamil.com>
2024-07-04 17:19:29 +0600

    Added Mutation delete Game and add new game

--- schema.js
+++ schema.js
@@ -26,4 +26,14 @@ type Query {
     authors: [Author]
     author(id: ID!): Author
 }
+
+type Mutation {
+    addGame(game: AddGameInput!): Game
+    deleteGame (id: ID!): [Game]
+}
+input AddGameInput {
+    title: String!,
+    platform: [String!]!
+}
+
 `;
